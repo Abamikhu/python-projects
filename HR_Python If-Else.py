aae0245610d097b36aa47c1cb2cb9ae7366a23ad
jones.iii.robert.it.trainings@gmail.com <jones.iii.robert.it.trainings@gmail.com>
2022-12-06 03:26:54 +0000

    Commit

---
+++ HR_Python If-Else.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+
+# if n is even and between 2 - 5, print Not Weird
+if (n % 2) == 0 and (n>= 2) and (n<= 5):
+    print("Not Weird")
+# if n is even and between 6 - 20, print Weird    
+else:   # (odd and (2-5 or <20 | 
+    if (n % 2) == 0 and (n>= 6) and (n<= 20):
+            print("Weird")
+    else:  # grade must be C, D or F
+        if (n % 2) == 0 and (n>= 20):
+            print("Not Weird")
+        else: print("Weird")
